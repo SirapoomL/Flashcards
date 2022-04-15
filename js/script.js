ad0d07c0dedce0addb610ab4c7eb65b1b81c33ae
Ailury <sirapoomlee@gmail.com>
2022-04-15 17:32:52 +0700

    push data to backend

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyBp3pvUwAble03XywPcMWNv1mGHkkY5Gt4",
+   authDomain: "flashcards-22550.firebaseapp.com",
+   projectId: "flashcards-22550",
+   storageBucket: "flashcards-22550.appspot.com",
+   messagingSenderId: "619643608848",
+   appId: "1:619643608848:web:8751fc0ea1dbcef65b0f95"
+ };
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBp3pvUwAble03XywPcMWNv1mGHkkY5Gt4",
-  authDomain: "flashcards-22550.firebaseapp.com",
-  projectId: "flashcards-22550",
-  storageBucket: "flashcards-22550.appspot.com",
-  messagingSenderId: "619643608848",
-  appId: "1:619643608848:web:8751fc0ea1dbcef65b0f95"
-};
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+
+// import 'package:firebase_core/firebase_core.dart';
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// initializeApp(firebaseConfig);
+// await Firebase.initializeApp();
 
 import {
 	getFirestore,
@@ -26,8 +32,21 @@ import {
 	query,
 	setDoc,
 	deleteDoc,
-} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+    
+    addDoc,
+    // collection,
+    // deleteDoc,
+    // doc,
+    // getDoc,
+    // getDocs,
+    // getFirestore,
+    updateDoc,
+} from "https://www.gstatic.com/firebasejs/9.6.11/firebase-firestore.js";
 
+
+const db = getFirestore();  
+// const db = firebase.firestore();
+const flashcards = collection(db, 'flashcards');
 
 //--------------------------------------------------------------------------------------------------------------
 
@@ -51,13 +70,34 @@ async function del(btn){
 //         row.parentNode.removeChild(row);
 }
 
-export async function addItem() {
-//     console.log('addItem');
-//     const start = await getDocs(flashcards);
-//     let pos = 0;
-//     for(pos=0;((start.docs[i].data()).name) != name;i++);
-//     const word = document.getElementById('word-to-add').value;
-//     const meaning = document.getElementById('meaning-to-add').value;
+export async function addItem(setid) {
+// //     console.log('addItem');
+    const wordset = await doc(db,`flashcards/${setid}`);
+    let wordInstance = await getDoc(wordset);
+    let Instance = wordInstance.data();
+    let word = document.getElementById('word-to-add').value;
+    let meaning = document.getElementById('meaning-to-add').value;
+    let toUpdate;
+    eval('toUpdate = { word'+Instance.length+' : word , meaning'+Instance.length+' : meaning, length : Instance.length+1};');
+    updateDoc(wordset, toUpdate);
+    // const collectionn = collection(db, `flashcards/${setid}`);
+    // let pos = 0;
+    // for(pos=0;((start.docs[i].data()).name) != name;i++);
+    // eval("var word"+Instance.length) = document.getElementById('word-to-add').value;
+    // window['word'+Instance.length]= document.getElementById('word-to-add').value;
+    // window['meaning'+Instance.length]= document.getElementById('meaning-to-add').value;
+    // eval("var word"+Instance.length + "= 'cat';");
+    // eval("var meaning"+Instance.length + "= document.getElementById('meaning-to-add').value;");
+    // const word = document.getElementById('word-to-add').value;
+    // const meaning = document.getElementById('meaning-to-add').value;
+    // if(database.docs[setid].data().length == 50)return;
+    // database.docs[setid].data().word[length] = word;
+    // database.docs[setid].data().meaning[length] = meaning;
+    // database.docs[setid].data().length = database.docs[setid].data().length+1;
+    // updateDoc(Instance.length,Instance.length+1);
+    // Instance = await getDoc(wordset);
+    // addDoc(flashcards,word);
+    // eval("updateDoc(wordset,{word["+Instance.length+"] : word"+Instance.length+",meaning["+Instance.length+"] : meaning"+Instance.length+"});");
 //     //ตรงนี้เอาไว้เช็คว่าidไหนคืออันล่าสุดที่ยังไม่ใส่
 //     for(let i=0;i<start.docs.length;i++){
 //         let x =false
@@ -73,9 +113,26 @@ export async function addItem() {
 //             break
 //         }
 //     }
-// }
+}
 
-// export async function deleteItem(bookmark) {
+export async function deleteItem(setid,value) {
+        let i = value;
+        const wordset = await doc(db,`flashcards/${setid}`);
+        let wordInstance = await getDoc(wordset);
+        let Instance = wordInstance.data();
+        updateDoc(wordset,{
+            length : length - 1
+        })
+        while(i!=Instance.length){
+            const x = i+1;
+            eval(`updateDoc(wordset,{
+                word`+i+` : Instance.word`+x+`,
+                meaning`+i+` : Instance.meaning`+x+`
+            })`);
+            i = i+1;
+        }
+        var row = this.parentNode.parentNode;
+        row.parentNode.removeChild(row);
 //     console.log('deleteItem');
 //     const it = await getDocs(booksRef);
 //     const docId =""+ bookmark
@@ -94,8 +151,15 @@ async function addnewitem(){
     if(textinput1.value=="" && textinput2.value==""){return}
     
     //ใส่ค่าในfirebase และรอจนเสร็จ
-    await addItem();
-
+    await addItem("5M1JLKmEGPnlwDOVNT9o");
+    // let iddd = "5M1JLKmEGPnlwDOVNT9o";
+    const wordset = await doc(db,`flashcards/5M1JLKmEGPnlwDOVNT9o`);
+    let wordInstance = await getDoc(wordset);
+    let Instance = wordInstance.data();
+    // let word = document.getElementById('word-to-add').value;
+    // let meaning = document.getElementById('meaning-to-add').value;
+    // let toUpdate;
+    // eval('toUpdate = { word'+Instance.length+' : word , meaning'+Instance.length+' : meaning, length : Instance.length+1};');
     //สร้างelementต่างๆในตาราง พวก row (trมั้ง?) ช่องข้อมูลในrow (td?) ปุ่ม
     
     let row = tables.insertRow( rowid.rowIndex);
@@ -105,14 +169,27 @@ async function addnewitem(){
     let deletebutton  = document.createElement("button");
 
     deletebutton.innerText="delete"
-    deletebutton.value=check[check.length-1]//เป็น docid 
+    deletebutton.value=Instance.length-1//เป็น docid 
     
-    deletebutton.onclick=function(){//ตรงนี้เป็นฟังก์ชั่นลบrow+ลบข้อมูลในfirebase
+    // deletebutton.onclick=deleteItem('5M1JLKmEGPnlwDOVNT9o',deletebutton.value)//{//ตรงนี้เป็นฟังก์ชั่นลบrow+ลบข้อมูลในfirebase
         // deleteItem(newbutton.value);
-        var row = this.parentNode.parentNode;
-        row.parentNode.removeChild(row);
-        
-    }
+    //     let i = value;
+    //     const wordset = await doc(db,`flashcards/${setid}`);
+    //     let wordInstance = await getDoc(wordset);
+    //     let Instance = wordInstance.data();
+    //     updateDoc(wordset,{
+    //         length : length - 1
+    //     })
+    //     while(i!=Instance.length){
+    //         eval(`updateDoc(wordset,{
+    //             word`+i+` : word`+i+1+`,
+    //             meaning`+i+` : meaning`+i+1+`
+    //         })`);
+    //         i = i+1;
+    //     }
+    //     var row = this.parentNode.parentNode;
+    //     row.parentNode.removeChild(row);
+    // }
     deletebutton.setAttribute("class","delete-row")
     buttonbox.appendChild(deletebutton);
     //เติมค่าในแถวใหม่
@@ -122,10 +199,12 @@ async function addnewitem(){
     row.appendChild(newmeaning);
     row.appendChild(buttonbox);
     //reset ค่าที่กรอกไป
+    
+//    updateDoc(wordset, toUpdate);
    textinput1.value=""
    textinput2.value=""
    
-}   
+}
 
 export async function addnewiteminit(word, meaning,docid){
     let tables = document.getElementById("content");
